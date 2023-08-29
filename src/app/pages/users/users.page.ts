186e558cd0f91226e9f56976bf5b4de614e41b40
Dewanshu <dewanshu.singh@paavu.com>
2023-08-29 19:14:54 +0530

    App in progress

--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import UserList from '../../../json/ClientData/clientData.json';
+import UserList from '../../../json/ClientData/clientData.json';
 
 @Component({
   selector: 'app-users',
