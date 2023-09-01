6cee2f99bf1231abc6d44accd208b1f1ef01c0a9
Dewanshu <dewanshu.singh@paavu.com>
2023-09-01 12:12:00 +0530

    ion-app ui update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,23 +15,23 @@ export class AppComponent {
     public authService: AuthService,
     public route: Router,
     public commonService: CommonServiceService
-  ) {}
+  ) { }
 
-  toggleChange(event: any) {
-    console.log('event', event);
-    if (event.detail.checked) {
-      document.body.setAttribute('color-theme', 'dark');
-    } else {
-      document.body.removeAttribute('color-theme');
-    }
-  }
+  // toggleChange(event: any) {
+  //   console.log('event', event);
+  //   if (event.detail.checked) {
+  //     document.body.setAttribute('color-theme', 'dark');
+  //   } else {
+  //     document.body.removeAttribute('color-theme');
+  //   }
+  // }
 
-  closeMenu(): void {
-    console.log(this.isMenuClosed);
-    this.isMenuClosed = true;
-    console.log(this.isMenuClosed);
+  // closeMenu(): void {
+  //   console.log(this.isMenuClosed);
+  //   this.isMenuClosed = true;
+  //   console.log(this.isMenuClosed);
 
-  }
+  // }
   async logOut() {
     this.authService.signOut().then(() => {
       this.authService.isUserLogin = false;
@@ -39,4 +39,36 @@ export class AppComponent {
       this.commonService.removeItem('userData');
     });
   }
+
+
+
+  public appPages = [
+    { heading: 'Videos', url: '/videos', icon: 'videocam-outline' },
+    { heading: 'Devices', url: '/devices', icon: 'phone-portrait-outline' },
+    { heading: 'Playlist', url: '/playlist', icon: 'play-outline' },
+    { heading: 'Category', url: '/category', icon: 'apps-outline' },
+    { heading: 'Locations', url: '/locations', icon: 'location-outline' },
+    { heading: 'Campaign', url: '/campaign', icon: 'barcode-outline' },
+    {
+      heading: 'Inventory',
+      url: '/inventory',
+      icon: 'file-tray-stacked-outline',
+    },
+    { heading: 'Invoice', url: '/invoice', icon: 'newspaper-outline' },
+    {
+      heading: 'Playout Certificate',
+      url: '/certificates',
+      icon: 'ribbon-outline',
+    },
+    { heading: 'Users', url: '/users', icon: 'people-outline' },
+    { heading: 'Profile', url: '/profile', icon: 'person-outline' },
+
+    // { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+    // { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    // { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    // { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    // { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+  ];
+
 }
--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import UserList from '../../../json/ClientData/clientData.json';
+import UserList from '../../../json/ClientData/clientData.json';
 
 @Component({
   selector: 'app-users',
@@ -7,112 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.page.scss'],
 })
 export class UsersPage implements OnInit {
+  public userList: any;
+  public results: any;
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.userList = UserList.data;
+    this.results = this.userList;
+    console.log("userList === ", this.userList);
   }
-  public userList: any = [
-    {
-      "id": "78",
-      "itemName": "AGRI-CORPORATE",
-      "Description": "",
-      "Parent_ID": "77",
-      "Parent_Category_Name": null,
-      "sort_index": null,
-      "haveChild": false,
-      "videoCount": "0",
-      "playListCount": "3",
-      "locationCount": "0"
-    },
-    {
-      "id": "79",
-      "itemName": "PESTICIDES",
-      "Description": "",
-      "Parent_ID": "77",
-      "Parent_Category_Name": null,
-      "sort_index": null,
-      "haveChild": false,
-      "videoCount": "0",
-      "playListCount": "3",
-      "locationCount": "0"
-    },
-    {
-      "id": "64",
-      "itemName": "1. Speciality",
-      "Description": "",
-      "Parent_ID": "0",
-      "Parent_Category_Name": null,
-      "sort_index": "1. Speciality_",
-      "haveChild": true,
-      "videoCount": "5",
-      "playListCount": "7",
-      "locationCount": "4"
-    },
-    {
-      "id": "69",
-      "itemName": "Cardiologist",
-      "Description": "",
-      "Parent_ID": "64",
-      "Parent_Category_Name": "1. Speciality",
-      "sort_index": "1. Speciality_Cardiologist",
-      "haveChild": false,
-      "videoCount": "4",
-      "playListCount": "21",
-      "locationCount": "6"
-    },
-    {
-      "id": "618",
-      "itemName": "Dental",
-      "Description": "dental",
-      "Parent_ID": "64",
-      "Parent_Category_Name": "1. Speciality",
-      "sort_index": "1. Speciality_Dental",
-      "haveChild": false,
-      "videoCount": "1",
-      "playListCount": "7",
-      "locationCount": "0"
-    },
-    {
-      "id": "617",
-      "itemName": "Diagnostics",
-      "Description": "Diagnostics",
-      "Parent_ID": "64",
-      "Parent_Category_Name": "1. Speciality",
-      "sort_index": "1. Speciality_Diagnostics",
-      "haveChild": false,
-      "videoCount": "0",
-      "playListCount": "4",
-      "locationCount": "0"
-    },
-    {
-      "id": "3",
-      "itemName": "General Physician",
-      "Description": "",
-      "Parent_ID": "64",
-      "Parent_Category_Name": "1. Speciality",
-      "sort_index": "1. Speciality_General Physician",
-      "haveChild": false,
-      "videoCount": "5",
-      "playListCount": "66",
-      "locationCount": "44"
-    },
-    {
-      "id": "4",
-      "itemName": "Gynaecologist",
-      "Description": "",
-      "Parent_ID": "64",
-      "Parent_Category_Name": "1. Speciality",
-      "sort_index": "1. Speciality_Gynaecologist",
-      "haveChild": false,
-      "videoCount": "33",
-      "playListCount": "138",
-      "locationCount": "129"
-    }
-  ];
 
-  // public userList = UserList
-  public results = [...this.userList];
 
   handleInput(event: any) {
     const query = event.target.value.toLowerCase();
