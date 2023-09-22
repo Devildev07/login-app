51f2c2b528cc9d2971d4d33421f2ef8ff4efad19
Devildev07 <dewanshusingh21@gmail.com>
2023-09-22 16:38:20 +0530

    in progress

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
 import { CommonServiceService } from 'src/app/common-service.service';
+import { GetDataService } from 'src/app/otherServices/get-data.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,17 +11,38 @@ import { CommonServiceService } from 'src/app/common-service.service';
 })
 export class ProfilePage implements OnInit {
   searchTxt: any = '';
+  getProfileData: any;
+  loggedUser: any = [
+    // {
+    //   address: '',
+    //   uname: '',
+    //   email: '',
+    //   mobile: '',
+    //   password: '',
+    //   region: '',
+    //   id: '',
+    // },
+  ];
+
   constructor(
     public authService: AuthService,
     public route: Router,
-    public common: CommonServiceService
+    public common: CommonServiceService,
+    public getDatas: GetDataService
   ) {
     var localdata = this.common.getItem('userData');
     console.log('localdata== ', localdata);
+
+    // this.getDatas.myEventEmitter.subscribe((data) => {
+    //   this.getProfileData.push(data);
+    //   console.log('Received event with data:', data);
+    // });
   }
 
-  ngOnInit() {}
- 
+  ngOnInit() {
+    this.getProfile();
+  }
+
   public data = this.authService.userEmail;
 
   async logOut() {
@@ -29,5 +51,16 @@ export class ProfilePage implements OnInit {
       this.route.navigate(['/login']);
       this.authService.isUserLogin = false;
     });
+  }
+
+  async getProfile() {
+    this.getProfileData = await this.getDatas.getFromFirebase('admins');
+    console.log('this is the profile  data', this.data);
+    console.log('getpProfileData === ', this.getProfileData);
+    let temp = this.getProfileData.filter(
+      (item: any) => item.email == this.data
+    );
+    this.loggedUser = temp;
+    console.log('loggedUser', this.loggedUser);
   }
 }
