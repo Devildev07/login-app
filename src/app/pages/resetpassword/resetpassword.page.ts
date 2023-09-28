59e4af7dc07f25eb255a00399b60294060d9718b
Devildev07 <dewanshusingh21@gmail.com>
2023-09-28 18:23:37 +0530

    Reset password done

--- src/app/pages/resetpassword/resetpassword.page.ts
+++ src/app/pages/resetpassword/resetpassword.page.ts
@@ -11,8 +11,11 @@ import { AuthService } from 'src/app/auth.service';
 export class ResetpasswordPage implements OnInit {
   resetForm: FormGroup | any;
   email: string | any;
-  constructor(public authService: AuthService, public route: Router,
-    public formBuilder: FormBuilder,) {}
+  constructor(
+    public authService: AuthService,
+    public route: Router,
+    public formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.resetForm = this.formBuilder.group({
@@ -24,20 +27,35 @@ export class ResetpasswordPage implements OnInit {
           Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
         ],
       ],
-   
     });
   }
 
   async resetPass() {
-    this.authService
-      .resetPass(this.email)
-      .then(() => {
-        console.log('reset link send');
+    if (this.resetForm.valid) {
+      // Get all form values
+      const formValues = this.resetForm.value;
+      console.log('Form Values:', formValues);
+      this.email = formValues['email'];
+      console.log('this.email', this.email);
+      this.authService
+        .resetPass(this.email)
+        .then(() => {
+          console.log('reset link send');
 
-        this.route.navigate(['/login']);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+          if (this.authService.isUserLogin === true) {
+            this.route.navigate(['/profile']);
+          } else {
+            this.route.navigate(['/login']);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(`Error occurs while sending link ${error}`);
+        });
+      // Now, you can access formValues.email to get the email value if needed.
+    } else {
+      // Handle form validation errors
+      console.log('Form is invalid.');
+    }
   }
 }
