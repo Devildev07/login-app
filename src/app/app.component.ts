a3b34a96a5caf3ee5fe043c205b7cb3e6d6b884a
Dewanshu Singh <dewanshusingh21@gmail.com>
2023-10-16 23:44:58 +0530

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  menuItems: any;
+  // menuItems: any;
   isMenuClosed = false;
 
   constructor(
--- src/app/common-service.service.ts
+++ src/app/common-service.service.ts
@@ -35,29 +35,29 @@ export class CommonServiceService {
     localStorage.removeItem(key);
   }
 
-  menuItem() {
-    return [
-      { heading: 'Videos', url: '/videos', icon: 'videocam-outline' },
-      { heading: 'Devices', url: '/devices', icon: 'phone-portrait-outline' },
-      { heading: 'Playlist', url: '/playlist', icon: 'play-outline' },
-      { heading: 'Category', url: '/category', icon: 'apps-outline' },
-      { heading: 'Locations', url: '/locations', icon: 'location-outline' },
-      { heading: 'Campaign', url: '/campaign', icon: 'barcode-outline' },
-      {
-        heading: 'Inventory',
-        url: '/inventory',
-        icon: 'file-tray-stacked-outline',
-      },
-      { heading: 'Invoice', url: '/invoice', icon: 'newspaper-outline' },
-      {
-        heading: 'Playout Certificate',
-        url: '/certificates',
-        icon: 'ribbon-outline',
-      },
-      { heading: 'Users', url: '/users', icon: 'people-outline' },
-      { heading: 'Profile', url: '/profile', icon: 'person-outline' },
-    ];
-  }
+  // menuItem() {
+  //   return [
+  //     { heading: 'Videos', url: '/videos', icon: 'videocam-outline' },
+  //     { heading: 'Devices', url: '/devices', icon: 'phone-portrait-outline' },
+  //     { heading: 'Playlist', url: '/playlist', icon: 'play-outline' },
+  //     { heading: 'Category', url: '/category', icon: 'apps-outline' },
+  //     { heading: 'Locations', url: '/locations', icon: 'location-outline' },
+  //     { heading: 'Campaign', url: '/campaign', icon: 'barcode-outline' },
+  //     {
+  //       heading: 'Inventory',
+  //       url: '/inventory',
+  //       icon: 'file-tray-stacked-outline',
+  //     },
+  //     { heading: 'Invoice', url: '/invoice', icon: 'newspaper-outline' },
+  //     {
+  //       heading: 'Playout Certificate',
+  //       url: '/certificates',
+  //       icon: 'ribbon-outline',
+  //     },
+  //     { heading: 'Users', url: '/users', icon: 'people-outline' },
+  //     { heading: 'Profile', url: '/profile', icon: 'person-outline' },
+  //   ];
+  // }
 
   encryptPass(getPass: string) {
     var passA = btoa(getPass);
