09ea0d68e5d32c313bc1589a04510cd9154afd5d
Dewanshu <dewanshu.singh@paavu.com>
2023-11-29 17:55:33 +0530

    master playlist in progress

--- src/app/pages/playlist/playlistTabs/master-playlist/master-playlist.page.ts
+++ src/app/pages/playlist/playlistTabs/master-playlist/master-playlist.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { CommonServiceService } from 'src/app/common-service.service';
+import { GetDataService } from 'src/app/otherServices/get-data.service';
+
 
 
 @Component({
@@ -8,10 +11,18 @@ import { CommonServiceService } from 'src/app/common-service.service';
   styleUrls: ['./master-playlist.page.scss'],
 })
 export class MasterPlaylistPage implements OnInit {
+  getMasterPlaylistData: any;
 
-  constructor(public CommonService: CommonServiceService) { }
+  constructor(public CommonService: CommonServiceService, public getDatas: GetDataService, public firestore: Firestore) { }
 
   ngOnInit() {
+    this.getMasterPlaylist()
+    this.CommonService.searchText = '';
+  }
+
+  async getMasterPlaylist() {
+    this.getMasterPlaylistData = await this.getDatas.getFromFirebase('playlist');
+    console.log("getMasterPlaylistData", this.getMasterPlaylistData);
   }
 
 }
