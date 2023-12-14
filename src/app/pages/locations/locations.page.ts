ab28a2941e0803a932fd2bd742528bdcbd4c0fbe
Dewanshu Singh <dewanshusingh21@gmail.com>
2023-12-14 18:58:40 +0530

    location in working

--- src/app/pages/locations/locations.page.ts
+++ src/app/pages/locations/locations.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { CommonServiceService } from 'src/app/common-service.service';
 
 @Component({
   selector: 'app-locations',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./locations.page.scss'],
 })
 export class LocationsPage implements OnInit {
+  public results: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public common: CommonServiceService,
+    public authService: AuthService
+  ) {
+    this.common.searchText = '';
   }
 
+  ngOnInit() {
+    this.common.searchText = '';
+  }
+
+  handleInput(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.common.searchText = query;
+  }
+
+  onClear() {
+    this.results = [];
+  }
 }
